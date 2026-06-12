b676d362707cfacf1e16c5d41b51b657cb6f9fab
Djianyoy <alifzildjian06@gmail.com>
2026-06-12 21:00:03 +0700

    third commit

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -31,6 +31,7 @@ export default function Navbar() {
           {/* Search */}
           <form onSubmit={handleSearch} className="flex-1 max-w-sm hidden md:flex">
             <div className="relative w-full">
+              
               <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
               <input
                 type="text"
